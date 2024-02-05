e1de15c11a3b1102da2e290d0050c62f4848b5a5
imdanaa <jangs1920@gmail.com>
2024-02-05 14:34:35 +0900

    [feat] 로그인 화면 완성 (#3)

--- Sluv2/Sluv2/Views/LoginVC.swift
+++ Sluv2/Sluv2/Views/LoginVC.swift
@@ -6,11 +6,79 @@
 //
 
 import UIKit
+import SnapKit
 
 class LoginVC: BaseController {
     // MARK: - Properties
     // 변수 및 상수, IBOutlet
+    
+    // MARK: [For UI Components]
+    let sluvLogo: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "Sluv_logo"))
+        
+        return imageView
+    }()
 
+    let explainLabel1: UILabel = {
+        let label = UILabel()
+        
+        label.text = "셀럽 아이템 정보 집합소"
+        label.font = .Heading2
+        label.textColor = UIColor(named: "Font-primary")
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        
+        return label
+    }()
+    
+    let explainLabel2: UILabel = {
+        let label = UILabel()
+        
+        label.text = "나누고 싶은 셀럽의 정보를\n우리만의 아지트에서!"
+        label.font = .Body3
+        label.textColor = UIColor(named: "Font-secondary")
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        
+        return label
+    }()
+    
+    let kakaoBtn: UIButton = {
+        let button = UIButton()
+        
+        button.setImage(UIImage(named: "Kakao_logo"), for: .normal)
+        
+        return button
+    }()
+    
+    let googleBtn: UIButton = {
+        let button = UIButton()
+        
+        button.setImage(UIImage(named: "Google_logo"), for: .normal)
+        
+        return button
+    }()
+    
+    let appleBtn: UIButton = {
+        let button = UIButton()
+        
+        button.setImage(UIImage(named: "Apple_logo"), for: .normal)
+        
+        return button
+    }()
+    
+    lazy var btnStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [kakaoBtn, googleBtn, appleBtn])
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.alignment = .center
+        stackView.distribution = .equalSpacing
+        stackView.spacing = 20
+        
+        return stackView
+    }()
+    
     // MARK: - Lifecycle
     // 생명주기와 관련된 메서드 (viewDidLoad, viewDidDisappear...)
     override func viewDidLoad() {
@@ -23,9 +91,38 @@ class LoginVC: BaseController {
     }
     
     override func addview() {
+        view.addSubview(sluvLogo)
+        view.addSubview(explainLabel1)
+        view.addSubview(explainLabel2)
+        view.addSubview(btnStackView)
     }
     
     override func layout() {
+        sluvLogo.snp.makeConstraints{ (a) in
+            a.width.equalTo(202)
+            a.height.equalTo(77)
+            a.centerX.equalToSuperview()
+            a.top.equalTo(view.safeAreaLayoutGuide).offset(150)
+        }
+        
+        explainLabel1.snp.makeConstraints{ (a) in
+            a.width.greaterThanOrEqualTo(168.67)
+            a.height.equalTo(22)
+            a.centerX.equalToSuperview()
+            a.top.equalTo(sluvLogo.snp.bottom).offset(32)
+        }
+
+        explainLabel2.snp.makeConstraints{ (a) in
+            a.width.greaterThanOrEqualTo(154)
+            a.height.equalTo(46)
+            a.centerX.equalToSuperview()
+            a.top.equalTo(explainLabel1.snp.bottom).offset(12)
+        }
+        
+        btnStackView.snp.makeConstraints{ (a) in
+            a.centerX.equalToSuperview()
+            a.bottom.equalTo(view.safeAreaLayoutGuide).offset(-60)
+        }
     }
     
     // MARK: - Helpers

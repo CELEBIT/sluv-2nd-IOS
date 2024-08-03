8ed6969d89942274de8e6c89d6a329f7c2a6ca91
imdanaa <jangs1920@gmail.com>
2024-08-04 02:47:02 +0900

    [feat] fcm 토큰 발급 작업 완료 (#57)

--- Sluv2/Sluv2/App/AppDelegate.swift
+++ Sluv2/Sluv2/App/AppDelegate.swift
@@ -13,6 +13,9 @@ import KakaoSDKAuth // 사용자 인증 및 토큰 관리 모듈
 import KakaoSDKUser // 카카오 로그인 모듈
 import GoogleSignIn
 import AuthenticationServices
+// push alarm
+import Firebase
+import FirebaseMessaging
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -123,6 +126,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         } else {
             print("저장된 토큰 없음. 로그인 화면으로!")
         }
+        
+        // MARK: - Firebase 설정
+        FirebaseApp.configure()
+        
+        // 앱 실행 시 사용자에게 알림 허용 권한을 받음
+        UNUserNotificationCenter.current().delegate = self
+        
+        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound] // 필요한 알림 권한을 설정
+        UNUserNotificationCenter.current().requestAuthorization(
+            options: authOptions,
+            completionHandler: { _, _ in }
+        )
+        
+        // UNUserNotificationCenterDelegate를 구현한 메서드를 실행시킴
+        application.registerForRemoteNotifications()
+        
+        // 파이어베이스 Meesaging 설정
+        Messaging.messaging().delegate = self
 
         return true
     }
@@ -186,5 +207,36 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
     }
 
+}
+
+// MARK: - Push Notification
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    
+    // 백그라운드에서 푸시 알림을 탭했을 때 실행
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        print("APNS token: \(deviceToken)")
+        Messaging.messaging().apnsToken = deviceToken
+    }
+    
+    // Foreground(앱 켜진 상태)에서도 알림 오는 설정
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        if #available(iOS 14.0, *) {
+            completionHandler([.list, .banner])
+        } else {
+            // Fallback on earlier versions
+        }
+    }
+}
+
+extension AppDelegate: MessagingDelegate {
+    
+    // 파이어베이스 MessagingDelegate 설정
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+      print("Firebase registration token: \(String(describing: fcmToken))")
+        
+      // TODO: If necessary send token to application server.
+      
+    }
 }
 
